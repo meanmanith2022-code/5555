2e6e67f3e6edecc0a07352c36564b6e8719574aa
Mean Manith <meanmanith2022@gmail.com>
2026-08-15 14:29:27 +0700

    Update main.py port configuration

--- meanney_backend/main.py
+++ meanney_backend/main.py
@@ -672,7 +672,6 @@ demo = gr.Interface(
     title="Auto Process Video Translation with AI"
 )
 
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
